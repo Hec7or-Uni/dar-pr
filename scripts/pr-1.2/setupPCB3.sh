d7ba19126c1d44e21b11ffeeb0f35b49c0e3fd24
fernando-lahoz <800989@unizar.es>
2023-10-16 15:22:03 +0200

    ifdown/ifup PCA/B3

--- scripts/pr-1.2/setupPCB3.sh
+++ scripts/pr-1.2/setupPCB3.sh
@@ -2,6 +2,15 @@
 
 # SETUP PCB3
 
+service zebra stop
+service ripd stop
+
+ifdown eth0
+ifup eth0
+
+ifdown eth1
+ifup eth1
+
 ip -4 addr add 192.168.20.3/24 broadcast 192.168.10.255 dev eth0
 
 ip -4 addr add 192.168.7.20/24 broadcast 192.168.7.255 dev eth1
@@ -9,9 +18,6 @@ ip -4 addr add 192.168.7.20/24 broadcast 192.168.7.255 dev eth1
 sysctl -w net.ipv4.ip_forward=1
 
 ip route add 192.168.10.0/24 via 192.168.7.10
-
-service zebra stop
-service ripd stop
 
 ip -4 link set eth0 mtu 1500
 ip -4 link set eth1 mtu 1500
