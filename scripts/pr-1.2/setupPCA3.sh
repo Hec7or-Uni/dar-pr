9e6f6358e032244aeff41daae482981814f7e2bc
fernando-lahoz <800989@unizar.es>
2023-10-16 15:16:48 +0200

    scripts 1.2 routers

--- scripts/pr-1.2/setupPCA3.sh
+++ scripts/pr-1.2/setupPCA3.sh
@@ -4,32 +4,14 @@
 
 ip -4 addr add 192.168.10.3/24 broadcast 192.168.20.255 dev eth0
 
-#ip -4 addr add 192.168.7.10/24 dev eth1
+ip -4 addr add 192.168.7.10/24 dev eth1
 
 sysctl -w net.ipv4.ip_forward=1
 
 ip route add 192.168.20.0/24 via 192.168.7.20
 
-#Direccionamiento dinamico
-tee /etc/quagga/zebra.conf > /dev/null << EOF
-hostname Router
-password zebra
-enable password zebra
+service zebra stop
+service ripd stop
 
-interface eth1
-no shutdown
-ip address 192.168.7.10/24
-EOF
-
-tee /etc/quagga/ripd.conf > /dev/null << EOF
-hostname ripd
-password zebra
-
-router rip
-
-network eth0
-network eth1
-EOF
-
-service zebra start
-service ripd start
+ip -4 link set eth0 mtu 1500
+ip -4 link set eth1 mtu 1500
