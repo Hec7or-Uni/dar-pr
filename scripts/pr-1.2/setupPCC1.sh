436e440ad7c70f4000648bd0bdbf8ba56f5982c0
fernando-lahoz <800989@unizar.es>
2023-10-16 15:45:30 +0200

    quitar sudo

--- scripts/pr-1.2/setupPCC1.sh
+++ scripts/pr-1.2/setupPCC1.sh
@@ -8,6 +8,6 @@ service ripd stop
 ifdown eth0
 ifup eth0
 
-sudo ip -4 addr add 192.168.7.1/24 broadcast 192.168.7.255 dev eth0
+ip -4 addr add 192.168.7.1/24 broadcast 192.168.7.255 dev eth0
 
-sudo ip route add 0.0.0.0/0 via 192.168.7.3
+ip route add 0.0.0.0/0 via 192.168.7.3
