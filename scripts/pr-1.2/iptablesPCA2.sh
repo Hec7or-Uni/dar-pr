e353a4f0c1ef6adad24e7817a099c9930c51830e
fernando-lahoz <800989@unizar.es>
2023-10-19 14:43:36 +0200

    asdjoaisd

--- scripts/pr-1.2/iptablesPCA2.sh
+++ scripts/pr-1.2/iptablesPCA2.sh
@@ -5,5 +5,7 @@
 #flush
 iptables -f
 
-iptables -t nat -A POSTROUTING -o eth0 -p tcp --dport 22 -j SNAT --to 192.168.20.1:2016
-iptables -t nat -A POSTROUTING -o eth0 -p tcp --dport 22 -j SNAT --to 192.168.20.2:2016
+iptables -t nat -A POSTROUTING -o eth1 -j SNAT --to 192.168.7.20 
+
+iptables -t nat -A POSTROUTING -o eth1 -p tcp --dport 22 -j SNAT --to 192.168.20.1:2016
+iptables -t nat -A POSTROUTING -o eth1 -p tcp --dport 22 -j SNAT --to 192.168.20.2:2016
