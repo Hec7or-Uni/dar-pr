0121ee616f10f6d4cec00bc6c8bd1ddcc7087689
sergioes55 <96990703+sergioes55@users.noreply.github.com>
2023-11-20 16:54:42 +0100

    Update hub.sh

--- scripts/pr3/hub.sh
+++ scripts/pr3/hub.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
-# Verificar si se proporciona un archivo como argumento
-if [ $# -eq 0 ]; then
-    echo "Por favor, proporciona un archivo como argumento."
+# Ruta a la carpeta que contiene los archivos .hub
+folder_path="/ruta/a/tu/carpeta"
+
+# Verificar si la carpeta existe
+if [ ! -d "$folder_path" ]; then
+    echo "La carpeta '$folder_path' no existe."
     exit 1
 fi
 
-# Leer el archivo proporcionado como argumento
-file="$1"
+# Bucle para procesar los archivos .hub en la carpeta
+for file in "$folder_path"/*.hub; do
+    if [ -f "$file" ]; then
+        echo "Procesando archivo: $file"
+        
+        # Tu código para procesar el archivo .hub en segundo plano
+        {
+            while IFS= read -r line; do
+                echo "Procesando línea: $line"
+                
+                # Dividir la línea en valores separados por espacios
+                values=($line)
+                
+                # Bucle para procesar cada valor de la línea
+                for val in "${values[@]}"; do
+                    echo "Valor: $val"
+                    # Aquí puedes realizar las acciones que necesites con cada valor
+                    # Por ejemplo, ejecutar algún comando o realizar algún cálculo
+                done
+            done < "$file"
+        } &
+    else
+        echo "No se encontraron archivos .hub en la carpeta."
+    fi
+done
 
-# Verificar si el archivo existe
-if [ ! -f "$file" ]; then
-    echo "El archivo '$file' no existe."
-    exit 1
-fi
-
-# Bucle para procesar cada línea del archivo
-while IFS= read -r line; do
-    echo "Procesando línea: $line"
-    
-    # Dividir la línea en valores separados por espacios
-    values=($line)
-    
-    # Bucle para procesar cada valor de la línea
-    for val in "${values[@]}"; do
-        echo "Valor: $val"
-        # Aquí puedes realizar las acciones que necesites con cada valor
-        # Por ejemplo, ejecutar algún comando o realizar algún cálculo
-    done
-done < "$file"
+# Esperar a que todos los procesos en segundo plano terminen
+wait
